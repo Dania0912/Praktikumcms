cf3d8e2ac5b3551a20570f8f2d371b1e2896c1bf
Dania0912 <231210023@student.mercubuana-yogya.ac.id>
2025-04-21 23:09:52 +0700

    Initial praktikumcms_database

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Karyawan;
+use App\Models\Penggajian;
+use App\Models\HR;
+use App\Models\JadwalKerja;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    public function index()
+    {
+        $karyawanCount = Karyawan::count();
+        $penggajianCount = Penggajian::count();
+        $hrCount = HR::count();
+        $jadwalKerjaCount = JadwalKerja::count();
+
+        return view('home', compact('karyawanCount', 'penggajianCount', 'hrCount', 'jadwalKerjaCount'));
+    }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,60 @@
-<!-- resources/views/home.blade.php -->
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <h3>Welcome to the Home Page!</h3>
-    <p>This is the home page of your application using the Sneat Bootstrap 5 Admin Template.</p>
+    <h1 class="text-center mb-4">Welcome to the Travel Explore Dashboard</h1>
 
-    <div class="card">
-        <h5 class="card-header">Welcome Message</h5>
-        <div class="card-body">
-            <p class="card-text">We are happy to have you using our template. You can customize this page and add more components as per your need.</p>
+    <!-- Stats Card Section -->
+    <div class="row">
+        <!-- Karyawan -->
+        <div class="col-lg-3 col-md-6 mb-4">
+            <div class="card text-center">
+                <div class="card-body">
+                    <h5 class="card-title">Karyawan</h5>
+                    <p class="card-text">
+                        {{ $karyawanCount ?? 2 }} Karyawan
+                    </p>
+                    <a href="{{ route('karyawan.index') }}" class="btn btn-primary">Lihat Karyawan</a>
+                </div>
+            </div>
+        </div>
+
+        <!-- Penggajian -->
+        <div class="col-lg-3 col-md-6 mb-4">
+            <div class="card text-center">
+                <div class="card-body">
+                    <h5 class="card-title">Penggajian</h5>
+                    <p class="card-text">
+                        {{ $penggajianCount ?? 2 }} Penggajian
+                    </p>
+                    <a href="{{ route('penggajian.index') }}" class="btn btn-primary">Lihat Penggajian</a>
+                </div>
+            </div>
+        </div>
+
+        <!-- HR -->
+        <div class="col-lg-3 col-md-6 mb-4">
+            <div class="card text-center">
+                <div class="card-body">
+                    <h5 class="card-title">HR</h5>
+                    <p class="card-text">
+                        {{ $hrCount ?? 2 }} HR
+                    </p>
+                    <a href="{{ route('hr.index') }}" class="btn btn-primary">Lihat HR</a>
+                </div>
+            </div>
+        </div>
+
+        <!-- Jadwal Kerja -->
+        <div class="col-lg-3 col-md-6 mb-4">
+            <div class="card text-center">
+                <div class="card-body">
+                    <h5 class="card-title">Jadwal Kerja</h5>
+                    <p class="card-text">
+                        {{ $jadwalKerjaCount ?? 2 }} Jadwal Kerja
+                    </p>
+                    <a href="{{ route('jadwalkerja.index') }}" class="btn btn-primary">Lihat Jadwal Kerja</a>
+                </div>
+            </div>
         </div>
     </div>
-</div>
 @endsection
